c8c260b3f368e11ec6df7cb5020005bf54b47e3c
杉岡成哉 <a1209s518n928@gmail.com>
2021-10-23 13:22:00 +0900

    Initial Commit

---
+++ Samidare-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2021/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Samidare-iOS/Samidare_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Samidare_iOSApp.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Samidare_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
