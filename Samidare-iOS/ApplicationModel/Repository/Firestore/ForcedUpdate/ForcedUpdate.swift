d7bad1709743e8021f6268aaefd4e399664f9889
杉岡成哉 <a1209s518n928@gmail.com>
2021-11-13 15:22:59 +0900

    アプリのバージョンと比較するように修正

--- Samidare-iOS/ApplicationModel/Repository/Firestore/ForcedUpdate/ForcedUpdate.swift
+++ Samidare-iOS/ApplicationModel/Repository/Firestore/ForcedUpdate/ForcedUpdate.swift
@@ -13,9 +13,9 @@ class ForcedUpdate {
     let document = Firestore.firestore().collection("version").document("forced_update")
     
     func shouldForcedUpdate() async throws -> Bool {
+        let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
         let data = try await document.getDocument().data(as: ForcedUpdateEntity.self)
-        print("testtt:\(data?.requiredVersion)")
-        //TODO: バージョンを確認する処理追加
-        return false
+        guard let data = data, let appVersion = appVersion else { return true }
+        return appVersion >= data.requiredVersion
     }
 }
