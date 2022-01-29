3c8bc24edba4f6ae6efaaca7556a14f7146a5734
杉岡成哉 <a1209s518n928@gmail.com>
2022-01-29 13:41:47 +0900

    残りの質問数Text追加

--- Samidare-iOS/QuestionModule/QuestionView.swift
+++ Samidare-iOS/QuestionModule/QuestionView.swift
@@ -13,6 +13,12 @@ struct QuestionView: View {
         // swiftlint:disable closure_body_length
         GeometryReader { geometry in
             VStack(spacing: 40) {
+                if presenter.status != .standBy && presenter.status != .ready {
+                    Text("\(presenter.selectIndex + 1)/\(presenter.totalQuestionCount)")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.textBlack)
+                }
                 ZStack {
                     TimerProgressBar(duration: $presenter.duration, color: presenter.status == .ready || presenter.status == .stopReadying ? .orangered : .bassBlue)
                     if presenter.status == .ready || presenter.status == .stopReadying {
