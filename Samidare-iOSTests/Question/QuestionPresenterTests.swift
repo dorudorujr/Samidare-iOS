8b428d7078eb032316dc042eadd7b186dbc1fe6c
杉岡成哉 <a1209s518n928@gmail.com>
2022-02-05 13:20:20 +0900

    testPrimaryButtonAction追加

--- Samidare-iOSTests/Question/QuestionPresenterTests.swift
+++ Samidare-iOSTests/Question/QuestionPresenterTests.swift
@@ -20,24 +20,100 @@ class QuestionPresenterTests: XCTestCase {
         appConfigRepositoryMock.getHandler = {
             .init(gameType: .init(name: "gameType"),
                   questionGroup: .init(name: "questionGroup"),
-                  time: 2)
+                  time: 1)
         }
         questionRepositoryMock.getQuestionsHandler = { _ in
             [
-                .init(body: "好きな色は", group: .init(name: "default")),
-                .init(body: "将来の夢は", group: .init(name: "default"))
+                .init(body: "好きな色は", group: .init(name: "default"))
             ]
         }
-        let interactory = try! QuestionInteractor(appConfigRepository: appConfigRepositoryMock,
-                                                  questionRepository: questionRepositoryMock)
-        presenter = .init(interactor: interactory)
+        setPresenter()
     }
     
     func testViewWillApper() {
-        XCTAssertEqual(presenter.nowCountDownTime, 0.0)
         XCTAssertEqual(presenter.totalQuestionCount, 0)
         presenter.viewWillApper()
-        XCTAssertEqual(presenter.nowCountDownTime, 2.0)
-        XCTAssertEqual(presenter.totalQuestionCount, 2)
+        XCTAssertEqual(presenter.totalQuestionCount, 1)
+    }
+    
+    // PrimaryButtonActionの一連の流れをテスト
+    func testPrimaryButtonAction() {
+        // 初期化
+        presenter.viewWillApper()
+        
+        // カウントダウン中
+        XCTAssertEqual(presenter.status, .standBy)
+        XCTAssertEqual(presenter.nowCountDownTime, 3)
+        presenter.primaryButtonAction()
+        XCTAssertEqual(presenter.status, .ready)
+        let expCountDown = expectation(description: "カウントダウン中")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            MockTimer.timer?.fire()
+            expCountDown.fulfill()
+        }
+        wait(for: [expCountDown], timeout: 0.1)
+        XCTAssertEqual(presenter.nowCountDownTime, 2)
+        XCTAssertEqual(presenter.duration, CGFloat(2) / CGFloat(3))
+        
+        // ゲーム中(ready → playへ)
+        let expGoToPlay = expectation(description: "ゲーム中へ遷移")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            expGoToPlay.fulfill()
+        }
+        wait(for: [expGoToPlay], timeout: 0.1)
+        XCTAssertEqual(presenter.nowCountDownTime, 3)
+        XCTAssertEqual(presenter.status, .play)
+        XCTAssertEqual(presenter.selectIndex, 0)
+        
+        // ゲーム中(質問表示中)
+        let expPlay = expectation(description: "質問表示中")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            MockTimer.timer?.fire()
+            expPlay.fulfill()
+        }
+        wait(for: [expPlay], timeout: 0.1)
+        XCTAssertEqual(presenter.duration, CGFloat(1.0 - 0.1) / CGFloat(1.0))
+        
+        // ゲーム中(１つの質問を表示する時間を経過)
+        let expOverTime = expectation(description: "質問表示時間経過")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            // 残り0.9秒を経過させたい
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            MockTimer.timer?.fire()
+            expOverTime.fulfill()
+        }
+        wait(for: [expOverTime], timeout: 0.1)
+        XCTAssertEqual(presenter.selectIndex, 1)
+        
+        // ゲーム完了
+        let expDone = expectation(description: "ゲーム完了")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            MockTimer.timer?.fire()
+            expDone.fulfill()
+        }
+        wait(for: [expDone], timeout: 0.1)
+        XCTAssertEqual(presenter.duration, 1.0)
+        XCTAssertEqual(presenter.status, .done)
+        XCTAssertEqual(presenter.selectIndex, 0)
+    }
+    
+    // MARK: - Set Data
+    
+    private func makeInteractory() -> QuestionInteractor {
+        try! .init(appConfigRepository: appConfigRepositoryMock, questionRepository: questionRepositoryMock)
+    }
+    
+    private func setPresenter() {
+        presenter = .init(interactor: makeInteractory(), timerProvider: MockTimer.self)
     }
 }
