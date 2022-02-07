c9a2622827b4e5f0ca927d10fb9d691698ecf77a
杉岡成哉 <a1209s518n928@gmail.com>
2022-02-07 21:51:59 +0900

    コメント追加

--- Samidare-iOSTests/Question/QuestionPresenterTests.swift
+++ Samidare-iOSTests/Question/QuestionPresenterTests.swift
@@ -42,9 +42,12 @@ class QuestionPresenterTests: XCTestCase {
         presenter.viewWillApper()
         
         // カウントダウン中
+        /// ステータス確認
         XCTAssertEqual(presenter.status, .standBy)
+        /// 現在のカウントダウンタイムが初期表示であるか確認
         XCTAssertEqual(presenter.nowCountDownTime, 3)
         presenter.primaryButtonAction()
+        /// ステータス確認
         XCTAssertEqual(presenter.status, .ready)
         let expCountDown = expectation(description: "カウントダウン中")
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
@@ -52,7 +55,9 @@ class QuestionPresenterTests: XCTestCase {
             expCountDown.fulfill()
         }
         wait(for: [expCountDown], timeout: 0.1)
+        /// 現在のカウントダウンタイムが1秒進んでいることの確認
         XCTAssertEqual(presenter.nowCountDownTime, 2)
+        /// プログレスバーの範囲計算が正常に行われているか確認
         XCTAssertEqual(presenter.duration, CGFloat(2) / CGFloat(3))
         
         // ゲーム中(ready → playへ)
@@ -63,8 +68,11 @@ class QuestionPresenterTests: XCTestCase {
             expGoToPlay.fulfill()
         }
         wait(for: [expGoToPlay], timeout: 0.1)
+        /// 準備期間が終了したらカウントダウンタイムが初期化されているか確認
         XCTAssertEqual(presenter.nowCountDownTime, 3)
+        /// ステータス確認
         XCTAssertEqual(presenter.status, .play)
+        /// 質問のインデックスが初期表示か確認
         XCTAssertEqual(presenter.selectIndex, 0)
         
         // ゲーム中(質問表示中)
@@ -74,6 +82,8 @@ class QuestionPresenterTests: XCTestCase {
             expPlay.fulfill()
         }
         wait(for: [expPlay], timeout: 0.1)
+        /// プログレスバーの範囲計算が正常に行われているか確認
+        /// 0.1秒進んでいるか確認
         XCTAssertEqual(presenter.duration, CGFloat(1.0 - 0.1) / CGFloat(1.0))
         
         // ゲーム中(１つの質問を表示する時間を経過)
@@ -93,6 +103,7 @@ class QuestionPresenterTests: XCTestCase {
             expOverTime.fulfill()
         }
         wait(for: [expOverTime], timeout: 0.1)
+        /// 質問時間経過で次の質問に行っているか確認
         XCTAssertEqual(presenter.selectIndex, 1)
         
         // ゲーム完了
@@ -102,8 +113,11 @@ class QuestionPresenterTests: XCTestCase {
             expDone.fulfill()
         }
         wait(for: [expDone], timeout: 0.1)
+        /// ゲーム完了後はプログレスバーを全表示に
         XCTAssertEqual(presenter.duration, 1.0)
+        /// ステータス確認
         XCTAssertEqual(presenter.status, .done)
+        /// ゲーム完了後は一番最後に表示した質問を表示
         XCTAssertEqual(presenter.selectIndex, 0)
     }
     
