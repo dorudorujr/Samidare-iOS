24da071504cafdc411b0631e03c45ef83555963a
杉岡成哉 <a1209s518n928@gmail.com>
2022-03-07 19:07:44 +0900

    InteractorのTest追加

---
+++ Samidare-iOSTests/Config/ConfigInteractorTests.swift
@@ -0,0 +1,31 @@
+//
+//  ConfigInteractorTests.swift
+//  Samidare-iOSTests
+//
+//  Created by 杉岡成哉 on 2022/03/05.
+//
+
+import XCTest
+@testable import Samidare_iOS
+
+class ConfigInteractorTests: XCTestCase {
+    private var appConfigRepositoryMock: AppConfigRepositoryMock!
+    private var interactor: ConfigInteractor!
+    
+    override func setUp() {
+        super.setUp()
+        appConfigRepositoryMock = .init()
+        appConfigRepositoryMock.getHandler = {
+            .init(gameType: .init(name: "gameType"),
+                  questionGroup: .init(name: "questionGroup"),
+                  time: 10)
+        }
+        interactor = .init(appConfigRepository: appConfigRepositoryMock)
+    }
+    
+    func testGetAppConfig() {
+        XCTAssertEqual(try! interactor.getAppConfig().gameType.name, "gameType")
+        XCTAssertEqual(try! interactor.getAppConfig().questionGroup.name, "questionGroup")
+        XCTAssertEqual(try! interactor.getAppConfig().time, 10)
+    }
+}
