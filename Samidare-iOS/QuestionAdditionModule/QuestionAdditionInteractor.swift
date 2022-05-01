e5e982f30d086e3ba35ef92fec5622bdf29d3e5f
杉岡成哉 <a1209s518n928@gmail.com>
2022-05-01 23:35:06 +0900

    Interactor追加

---
+++ Samidare-iOS/QuestionAdditionModule/QuestionAdditionInteractor.swift
@@ -0,0 +1,31 @@
+//
+//  QuestionAdditionInteractor.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2022/05/01.
+//
+
+import Foundation
+
+class QuestionAdditionInteractor {
+    private let questionRepository: QuestionRepository
+    init(questionRepository: QuestionRepository = QuestionRepositoryImpl()) {
+        self.questionRepository = questionRepository
+    }
+    
+    func getQuestions(of group: String) -> [Question] {
+        questionRepository.getQuestions(of: group)
+    }
+    
+    func add(_ question: Question) throws {
+        try questionRepository.add(question)
+    }
+    
+    func update(_ question: Question) throws {
+        try questionRepository.update(question)
+    }
+    
+    func delete(_ question: Question, of group: String) throws {
+        try questionRepository.delete(question, of: group)
+    }
+}
