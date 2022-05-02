50bf28d3dcb7457658ff026ed175640d266495fc
杉岡成哉 <a1209s518n928@gmail.com>
2022-05-02 17:26:37 +0900

    QuestionAdditionView実装

---
+++ Samidare-iOS/QuestionAdditionModule/QuestionAdditionView.swift
@@ -0,0 +1,58 @@
+//
+//  QuestionAdditionView.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct QuestionAdditionView: View {
+    @ObservedObject private var presenter: QuestionAdditionPresenter
+    
+    init(presenter: QuestionAdditionPresenter) {
+        self.presenter = presenter
+    }
+    
+    // swiftlint:disable closure_body_length
+    var body: some View {
+        ZStack {
+            TextFieldAlertView(text: $presenter.addQuestionBody,
+                               isShowingAlert: $presenter.isShowingAddAlert,
+                               placeholder: "",
+                               isSecureTextEntry: false,
+                               title: L10n.Question.Addition.Alert.title,
+                               message: L10n.Question.Addition.Alert.message,
+                               leftButtonTitle: L10n.Common.cancel,
+                               rightButtonTitle: L10n.Common.add,
+                               leftButtonAction: nil,
+                               rightButtonAction: { presenter.addQuestion() })
+            List {
+                Section {
+                    if let questions = self.presenter.questions {
+                        ForEach(questions) { question in
+                            QuestionListCardView(questionBody: question.body)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(L10n.Group.Addition.title)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { presenter.didTapNavBarButton() }, label: {
+                        Image(systemName: "plus")
+                            .renderingMode(.template)
+                            .foregroundColor(.blue)
+                    })
+                }
+            }
+        }
+    }
+}
+
+struct QuestionAdditionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionAdditionView(presenter: .init(interactor: .init(), group: "デフォルト"))
+    }
+}
