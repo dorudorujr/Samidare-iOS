90f87f4858beb92f358c4ad1d63777c99ab9182e
杉岡成哉 <a1209s518n928@gmail.com>
2022-05-14 13:15:21 +0900

    コメント追加

--- Samidare-iOSTests/SnapshotTest/QuestionListViewTests.swift
+++ Samidare-iOSTests/SnapshotTest/QuestionListViewTests.swift
@@ -31,6 +31,7 @@ class QuestionListViewTests: XCTestCase {
     func testQuestionListViewStandard() {
         let questionListView = QuestionListView(presenter: questionListPresenter)
         let vc = UIHostingController(rootView: questionListView)
+        // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
         assertSnapshot(matching: vc, as: .image(on: .iPhoneXsMax, precision: 0.7))
     }
 
