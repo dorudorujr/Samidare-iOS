b109f3fae28a260dfcfceb1dc2080427c1e7a422
杉岡成哉 <a1209s518n928@gmail.com>
2022-05-20 22:54:51 +0900

    GroupAdditionTests追加

--- Samidare-iOSTests/SnapshotTest/GroupAdditionTests.swift
+++ Samidare-iOSTests/SnapshotTest/GroupAdditionTests.swift
@@ -5,29 +5,31 @@
 //  Created by 杉岡成哉 on 2022/04/23.
 //
 
-//import XCTest
-//import SnapshotTesting
-//import SwiftUI
-//@testable import Samidare_iOS
-//
-//class GroupAdditionTests: XCTestCase {
-//    private var questionGroupRepositoryMock: QuestionGroupRepositoryMock!
-//    private var presenter: GroupAdditionPresenter!
-//
-//    override func setUp() async throws {
-//        try await super.setUp()
-//        isRecording = false
-//        questionGroupRepositoryMock = .init()
-//        questionGroupRepositoryMock.getHandler = {
-//            [
-//                .init(name: "デフォルト（テスト）")
-//            ]
-//        }
-//        presenter = await .init(interactor: .init(questionGroupRepository: questionGroupRepositoryMock), router: .init())
-//    }
-//
-//    func testStandard() {
-//        let view = GroupAdditionView(presenter: presenter)
-//        assertSnapshot(matching: view.referenceFrame(), as: .image)
-//    }
-//}
+import XCTest
+import SnapshotTesting
+import SwiftUI
+@testable import Samidare_iOS
+
+class GroupAdditionTests: XCTestCase {
+    private var questionGroupRepositoryMock: QuestionGroupRepositoryMock!
+    private var presenter: GroupAdditionPresenter!
+
+    override func setUp() async throws {
+        try await super.setUp()
+        isRecording = false
+        questionGroupRepositoryMock = .init()
+        questionGroupRepositoryMock.getHandler = {
+            [
+                .init(name: "デフォルト（テスト）")
+            ]
+        }
+        presenter = await .init(interactor: .init(questionGroupRepository: questionGroupRepositoryMock), router: .init())
+    }
+
+    func testStandard() {
+        let view = GroupAdditionView(presenter: presenter)
+        let vc = UIHostingController(rootView: view)
+        // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
+        assertSnapshot(matching: vc, as: .image(on: .iPhoneXsMax, precision: 0.95))
+    }
+}
