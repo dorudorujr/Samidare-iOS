aab39890e3fb56948a8268f794b4a47a16094aea
杉岡成哉 <a1209s518n928@gmail.com>
2022-05-20 22:51:38 +0900

    ConfigViewTests追加

--- Samidare-iOSTests/SnapshotTest/ConfigViewTests.swift
+++ Samidare-iOSTests/SnapshotTest/ConfigViewTests.swift
@@ -5,32 +5,33 @@
 //  Created by 杉岡成哉 on 2022/03/07.
 //
 
-//import XCTest
-//import SnapshotTesting
-//import SwiftUI
-//@testable import Samidare_iOS
+import XCTest
+import SnapshotTesting
+import SwiftUI
+@testable import Samidare_iOS
 
-//class ConfigViewTests: XCTestCase {
-//    private var appConfigRepositoryMock: AppConfigRepositoryMock!
-//    private var presenter: ConfigPresenter!
-//
-//    override func setUp() async throws {
-//        try await super.setUp()
-//        isRecording = false
-//
-//        appConfigRepositoryMock = .init()
-//        appConfigRepositoryMock.getHandler = {
-//            .init(gameType: .init(name: "gameType"),
-//                  questionGroup: .init(name: "questionGroup"),
-//                  time: 1)
-//        }
-//
-//        presenter = await .init(interactor: .init(appConfigRepository: appConfigRepositoryMock), router: .init())
-//    }
-//
-//    func testStandard() {
-//        let view = ConfigView(presenter: presenter)
-//        assertSnapshot(matching: view.referenceFrame(),
-//                       as: .image(precision: 0.8))
-//    }
-//}
+class ConfigViewTests: XCTestCase {
+    private var appConfigRepositoryMock: AppConfigRepositoryMock!
+    private var presenter: ConfigPresenter!
+
+    override func setUp() async throws {
+        try await super.setUp()
+        isRecording = false
+
+        appConfigRepositoryMock = .init()
+        appConfigRepositoryMock.getHandler = {
+            .init(gameType: .init(name: "gameType"),
+                  questionGroup: .init(name: "questionGroup"),
+                  time: 1)
+        }
+
+        presenter = await .init(interactor: .init(appConfigRepository: appConfigRepositoryMock), router: .init())
+    }
+
+    func testStandard() {
+        let view = ConfigView(presenter: presenter)
+        let vc = UIHostingController(rootView: view)
+        // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
+        assertSnapshot(matching: vc, as: .image(on: .iPhoneXsMax, precision: 0.95))
+    }
+}
