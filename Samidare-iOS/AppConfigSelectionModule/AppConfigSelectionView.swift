dd3a50c708d72b85b9f6305edd93d54c08af3d46
杉岡成哉 <a1209s518n928@gmail.com>
2022-06-12 15:58:41 +0900

    AppConfigSelectionView追加

---
+++ Samidare-iOS/AppConfigSelectionModule/AppConfigSelectionView.swift
@@ -0,0 +1,37 @@
+//
+//  AppConfigSelectionView.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct AppConfigSelectionView<AppConfigRepository: AppConfigRepositoryProtocol, GroupRepository: QuestionGroupRepositoryProtocol>: View {
+    @ObservedObject private var presenter: AppConfigSelectionPresenter<AppConfigRepository, GroupRepository>
+    
+    init(presenter: AppConfigSelectionPresenter<AppConfigRepository, GroupRepository>) {
+        self.presenter = presenter
+    }
+    
+    var body: some View {
+        List {
+            Section {
+                ForEach(self.presenter.questionGroups) { group in
+                    ListRow(title: group.name, description: nil)
+                        .onTapGesture {
+                            presenter.update(group.name)
+                        }
+                }
+            }
+        }
+        .navigationTitle(L10n.App.Config.Selection.Question.Group.title)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AppConfigSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppConfigSelectionView<AppConfigRepositoryImpl, QuestionGroupRepositoryImpl>(presenter: .init(interactor: .init()))
+    }
+}
