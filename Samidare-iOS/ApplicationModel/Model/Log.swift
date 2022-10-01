35ebf3153c32c467e261a53194fcc4ecbe5a7e21
杉岡成哉 <a1209s518n928@gmail.com>
2022-10-01 14:36:15 +0900

    Logを出力する、Crashlyticsに送る機能実装

---
+++ Samidare-iOS/ApplicationModel/Model/Log.swift
@@ -0,0 +1,17 @@
+//
+//  Log.swift
+//  Samidare-iOS
+//
+//  Created by 杉岡成哉 on 2022/10/01.
+//
+
+import Firebase
+import os
+
+struct Log {
+    static func fault(_ error: Error) {
+        let logger = Logger(subsystem: "com.sugiokaseiya", category: "fault")
+        logger.fault("⚠️⚠️Error⚠️⚠️:\(error.localizedDescription)")
+        Crashlytics.crashlytics().record(error: error)
+    }
+}
