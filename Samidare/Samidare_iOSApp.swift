69eb4548b7f6b22fc4e9525b3aab949ec8ff593c
杉岡成哉 <a1209s518n928@gmail.com>
2022-10-08 20:45:37 +0900

    Mobile Ads SDKの初期追加

--- Samidare/Samidare_iOSApp.swift
+++ Samidare/Samidare_iOSApp.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import Firebase
+import GoogleMobileAds
 
 @main
 struct Samidare_iOSApp: App {
     init() {
         FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         RealmConfig.configure()
     }
     
