38d3fe3499dfd13a15111a3bfc92217b533da86d
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-02 19:04:03 +0900

    閾値を外してみる

--- SamidareTests/SnapshotTest/QuestionViewTests.swift
+++ SamidareTests/SnapshotTest/QuestionViewTests.swift
@@ -39,7 +39,7 @@ class QuestionViewTests: XCTestCase {
         let questionView = QuestionView<QuestionRepositoryProtocolMock, AppConfigRepositoryProtocolMock>(presenter: presenter)
         let vc = UIHostingController(rootView: questionView)
         assertSnapshot(matching: vc,
-                       as: .image(on: .iPhone13ProMax, precision: 0.95))
+                       as: .image(on: .iPhone13ProMax))
     }
 
     func testReady() {
@@ -48,7 +48,7 @@ class QuestionViewTests: XCTestCase {
         presenter.primaryButtonAction()
         let vc = UIHostingController(rootView: questionView)
         assertSnapshot(matching: vc,
-                       as: .image(on: .iPhone13ProMax, precision: 0.95))
+                       as: .image(on: .iPhone13ProMax))
     }
     
     func testStopReadying() {
@@ -58,7 +58,7 @@ class QuestionViewTests: XCTestCase {
         presenter.secondaryButtonAction()
         let vc = UIHostingController(rootView: questionView)
         assertSnapshot(matching: vc,
-                       as: .image(on: .iPhone13ProMax, precision: 0.95))
+                       as: .image(on: .iPhone13ProMax))
     }
     
     func testPlay() {
@@ -76,7 +76,7 @@ class QuestionViewTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         let vc = UIHostingController(rootView: questionView)
         assertSnapshot(matching: vc,
-                       as: .image(on: .iPhone13ProMax, precision: 0.95))
+                       as: .image(on: .iPhone13ProMax))
     }
     
     func testStopPlaying() {
@@ -95,7 +95,7 @@ class QuestionViewTests: XCTestCase {
         presenter.secondaryButtonAction()
         let vc = UIHostingController(rootView: questionView)
         assertSnapshot(matching: vc,
-                       as: .image(on: .iPhone13ProMax, precision: 0.95))
+                       as: .image(on: .iPhone13ProMax))
     }
     
     func testDone() {
