c5d2e485ed7c0f8496b9bbf2cbc593b419d1a9be
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-04 13:57:24 +0900

    画像を更新&閾値の精度を上げる

--- SamidareTests/SnapshotTest/QuestionAdditionViewTests.swift
+++ SamidareTests/SnapshotTest/QuestionAdditionViewTests.swift
@@ -32,6 +32,6 @@ class QuestionAdditionViewTests: XCTestCase {
         let view = QuestionAdditionView<QuestionRepositoryProtocolMock>(presenter: presenter)
         let vc = UIHostingController(rootView: view)
         // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
-        assertSnapshot(matching: vc, as: .image(on: .iPhone13ProMax, precision: 0.95))
+        assertSnapshot(matching: vc, as: .image(on: .iPhone13ProMax, precision: 0.999))
     }
 }
