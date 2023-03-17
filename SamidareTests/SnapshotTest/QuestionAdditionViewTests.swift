82096a3384dbc3859250772cc67e0940f4feb71e
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-17 19:41:15 +0900

    コメント追加

--- SamidareTests/SnapshotTest/QuestionAdditionViewTests.swift
+++ SamidareTests/SnapshotTest/QuestionAdditionViewTests.swift
@@ -41,6 +41,7 @@ class QuestionAdditionViewTests: XCTestCase {
         }
         let view = QuestionAdditionView(store: store)
         let vc = UIHostingController(rootView: view)
+        // 謎にリストが表示されないので一旦コメントアウト(ForEachが原因っぽい....)
         // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
         //assertSnapshot(matching: vc, as: .image(on: .iPhone13ProMax, precision: 0.999))
         //assertSnapshot(matching: vc, as: .wait(for: 20.0, on: .image(on: .iPhone13ProMax, precision: 0.999)))
