ae212d44c549268434a2dfd4a35f84b7709cd757
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-23 20:23:58 +0900

    Testのコンパイルエラーを修正

--- SamidareTests/SnapshotTest/GroupAdditionTests.swift
+++ SamidareTests/SnapshotTest/GroupAdditionTests.swift
@@ -7,27 +7,31 @@
 
 import XCTest
 import SnapshotTesting
-import SwiftUI
+import ComposableArchitecture
 @testable import Samidare
+import SwiftUI
 
 class GroupAdditionTests: XCTestCase {
-    private var questionGroupRepositoryMock: QuestionGroupRepositoryProtocolMock!
-    private var presenter: GroupAdditionPresenter<QuestionGroupRepositoryProtocolMock>!
+    private var questionGroupRepositoryMock = QuestionGroupRepositoryProtocolMock()
 
     override func setUp() async throws {
         try await super.setUp()
         isRecording = false
-        questionGroupRepositoryMock = .init()
-        QuestionGroupRepositoryProtocolMock.getHandler = {
+        questionGroupRepositoryMock.getQuestionGroupHandler = {
             [
                 .init(name: "デフォルト（テスト）")
             ]
         }
-        presenter = await .init(interactor: .init(), router: .init())
     }
 
     func testStandard() {
-        let view = GroupAdditionView<QuestionGroupRepositoryProtocolMock>(presenter: presenter)
+        let store = withDependencies {
+            $0.questionGroupRepository = questionGroupRepositoryMock
+        } operation: {
+            StoreOf<GroupAdditionReducer>(initialState: GroupAdditionReducer.State(),
+                    reducer: GroupAdditionReducer())
+        }
+        let view = GroupAdditionView(store: store)
         let vc = UIHostingController(rootView: view)
         // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
         assertSnapshot(matching: vc, as: .image(on: .iPhone13ProMax, precision: 0.999))
