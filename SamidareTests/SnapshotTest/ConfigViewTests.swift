72c8c67473a9d9f602c56d6cbbe63a45cda88af2
杉岡成哉 <a1209s518n928@gmail.com>
2023-03-27 11:44:02 +0900

    コンパイルエラー修正

--- SamidareTests/SnapshotTest/ConfigViewTests.swift
+++ SamidareTests/SnapshotTest/ConfigViewTests.swift
@@ -7,26 +7,31 @@
 
 import XCTest
 import SnapshotTesting
-import SwiftUI
+import ComposableArchitecture
 @testable import Samidare
+import SwiftUI
 
 class ConfigViewTests: XCTestCase {
-    private var presenter: ConfigPresenter<AppConfigRepositoryProtocolMock, QuestionGroupRepositoryProtocolMock>!
+    private var appConfigRepositoryMock = AppConfigRepositoryProtocolMock()
 
     override func setUp() async throws {
         try await super.setUp()
         isRecording = false
 
-        AppConfigRepositoryProtocolMock.getHandler = {
+        appConfigRepositoryMock.getAppConfigHandler = {
             .init(questionGroupName: "questionGroup",
                   time: 1)
         }
-
-        presenter = await .init(interactor: .init(), router: .init())
     }
 
     func testConfigViewTestStandard() {
-        let view = ConfigView<AppConfigRepositoryProtocolMock, QuestionGroupRepositoryProtocolMock>(presenter: presenter)
+        let store = withDependencies {
+            $0.appConfigRepository = appConfigRepositoryMock
+        } operation: {
+            StoreOf<ConfigReducer>(initialState: .init(),
+                    reducer: ConfigReducer())
+        }
+        let view = ConfigView(store: store)
         let vc = UIHostingController(rootView: view)
         // M1とCIとでSnapshotの画像に差異が発生するので閾値設定
         assertSnapshot(matching: vc, as: .image(on: .iPhone13ProMax, precision: 0.999))
